11f0031c4ffe54ea16575446b553b90ca0df3f5d
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-04-29 13:48:42 +0300

    Update Creator.vue

--- bitmap-editor/src/views/Creator.vue
+++ bitmap-editor/src/views/Creator.vue
@@ -1,14 +1,13 @@
 <template>
     <v-container class="gallery">
         <v-row>
-            <v-col cols="12" lg="12">
+            <v-col cols="12">
                 <v-bottom-navigation :value="pixelMode" color="primary">
-                    <v-btn @click="changePixelModeTo8x8()">
+                    <v-btn @click="pixelMode = 0">
                         <span>8x8 Pixel</span>
                         <v-icon>mdi-grid</v-icon>
                     </v-btn>
-
-                    <v-btn @click="changePixelModeTo8x32()">
+                    <v-btn @click="pixelMode = 1">
                         <span>8x32 Pixel</span>
                         <v-icon>mdi-grid</v-icon>
                     </v-btn>
@@ -20,37 +19,37 @@
                 <v-card class="pa-3" elevation="4">
                     <Art :colors="colors" pixelCount="64" :func="onclick" />
                     <p></p>
-                    <v-textarea filled outlined v-model="array8x8String" rows="5" hide-details></v-textarea>
+                    <v-textarea filled outlined v-model="array8x8String" rows="5" hide-details readonly label="Код массива 8x8"></v-textarea>
                 </v-card>
             </v-col>
             <v-col cols="12" lg="4">
                 <v-card class="pa-3" elevation="4">
-                    <v-color-picker v-model="colors" mode="hexa" dot-size="20" show-swatches swatches-max-height="280"></v-color-picker>
+                    <v-color-picker v-model="colors" mode="hexa" show-swatches swatches-max-height="280"></v-color-picker>
                 </v-card>
             </v-col>
         </v-row>
         <v-row v-if="pixelMode == 1">
-            <v-col cols="12" lg="8" offset-lg="0">
+            <v-col cols="12" lg="8">
                 <v-card class="pa-3" elevation="4">
                     <Art :colors="colors" pixelCount="256" :func="onclick" />
                     <p></p>
-                    <v-textarea filled outlined v-model="array8x32String" rows="9" hide-details></v-textarea>   
-                    <div class="text-center" v-if="isAnimated" >    
-                        <v-card-text><h3 class="red--text">No animated 8x32 bitmaps are supported!</h3></v-card-text>
-                    </div> 
+                    <v-textarea filled outlined v-model="array8x32String" rows="9" hide-details readonly label="Код массива 8x32"></v-textarea>
                 </v-card>
             </v-col>
             <v-col cols="12" lg="4">
                 <v-card class="pa-3" elevation="4">
-                    <v-color-picker v-model="colors" mode="hexa" dot-size="20" show-swatches swatches-max-height="270"></v-color-picker>
+                    <v-color-picker v-model="colors" mode="hexa" show-swatches swatches-max-height="270"></v-color-picker>
                 </v-card>
             </v-col>
         </v-row>
     </v-container>
 </template>
+
 <script>
-import Art from '../components/Art';
+import Art from './components/Art.vue';
+
 export default {
+    components: { Art },
     data() {
         return {
             colors: '#F44336',
@@ -61,89 +60,32 @@ export default {
             array8x32String: '',
         };
     },
-    components: {
-        Art,
-    },
-    computed: {
-        cleaned8x32String(){
-            return this.array8x32String.replaceAll('\n', '').replaceAll(' ', '');
-        },
-        isAnimated(){
-            return this.cleaned8x32String.includes('],[');
-        },
-    },
     methods: {
         onclick(id, color) {
-            if (Object.keys(this.active8x32Background).length < 256) {
-                for (let i = 1; i < 256; i++) {
-                    this.active8x32Background[i] = '000';
-                }
-            }
-
-            if (Object.keys(this.active8x8Background).length < 64) {
-                for (let i = 1; i < 64; i++) {
-                    this.active8x8Background[i] = '000';
-                }
-            }
-
+            const hexColor = color.replace('#', '');
             if (this.pixelMode == 0) {
-                this.active8x8Background[id] = color.replace('#', '');
-                this.array8x8String = '[';
-
-                for (const hex of Object.values(this.active8x8Background)) {
-                    this.array8x8String += rgb888ToRgb565(hexToRgb(hex)) + ',';
-                }
-
-                this.array8x8String = this.array8x8String.slice(0, -1);
-                this.array8x8String += ']';
+                this.active8x8Background[id] = hexColor;
+                this.array8x8String = this.generateArray(this.active8x8Background, 64);
             } else {
-                this.active8x32Background[id] = color.replace('#', '');
-                this.array8x32String = '[';
-
-                for (const hex of Object.values(this.active8x32Background)) {
-                    this.array8x32String += rgb888ToRgb565(hexToRgb(hex)) + ',';
-                }
-
-                this.array8x32String = this.array8x32String.slice(0, -1);
-                this.array8x32String += ']';
+                this.active8x32Background[id] = hexColor;
+                this.array8x32String = this.generateArray(this.active8x32Background, 256);
             }
         },
-        changePixelModeTo8x8() {
-            this.pixelMode = 0;
+        generateArray(storage, count) {
+            let res = '[';
+            for (let i = 0; i < count; i++) {
+                const hex = storage[i] || '000000';
+                res += this.rgb888ToRgb565(this.hexToRgb(hex)) + (i === count - 1 ? '' : ',');
+            }
+            return res + ']';
         },
-        changePixelModeTo8x32() {
-            this.pixelMode = 1;
+        rgb888ToRgb565(rgb) {
+            return ((rgb.r & 0xf8) << 8) + ((rgb.g & 0xfc) << 3) + (rgb.b >> 3);
         },
-    },
+        hexToRgb(hex) {
+            const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+            return result ? { r: parseInt(result[1], 16), g: parseInt(result[2], 16), b: parseInt(result[3], 16) } : { r:0, g:0, b:0 };
+        }
+    }
 };
-
-function rgb888ToRgb565(rgbArray) {
-    return ((rgbArray.r & 0xf8) << 8) + ((rgbArray.g & 0xfc) << 3) + (rgbArray.b >> 3);
-}
-
-function hexToRgb(hex) {
-    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function (m, r, g, b) {
-        return r + r + g + g + b + b;
-    });
-
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result
-        ? {
-              r: parseInt(result[1], 16),
-              g: parseInt(result[2], 16),
-              b: parseInt(result[3], 16),
-          }
-        : null;
-}
 </script>
-<style scoped>
-#art,
-.v-color-picker {
-    padding-left: 0;
-    padding-right: 0;
-    margin-left: auto;
-    margin-right: auto;
-    display: block;
-}
-</style>
