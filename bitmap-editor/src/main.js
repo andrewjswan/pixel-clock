e688f72345c8b0230d1d8bbadf04985594243529
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-04-29 13:54:28 +0300

    Delete bitmap-editor/src/main.js

--- bitmap-editor/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import Creator from './views/Creator.vue'
-
-Vue.use(Vuetify)
-
-Vue.component('bitmap-editor', Creator)
-
-export default Creator
