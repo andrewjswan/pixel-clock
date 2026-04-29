ee99dfb52e70e5870d76f0f8bc71e65e23398cac
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-04-29 13:57:19 +0300

    Update vue.config.js

--- bitmap-editor/vue.config.js
+++ bitmap-editor/vue.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  css: { extract: false },
+  css: {
+    extract: false
+  },
   chainWebpack: config => {
     config.module
       .rule('vue')
       .use('vue-loader')
       .tap(options => {
-        options.shadowMode = true;
-        return options;
-      });
+        options.shadowMode = true
+        return options
+      })
   }
-};
+}
