f81dac7a4a3ea13ba2d6cfd49d9141a1b9da6b3d
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-04-29 14:10:06 +0300

    Add `bitmap-editor` (#84)

---
+++ bitmap-editor/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  css: {
+    extract: false
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+        options.shadowMode = true
+        return options
+      })
+  }
+}
